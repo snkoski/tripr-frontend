64e8cbe739e869dfb855dd18f74ed7b58ccffcd7
ahjuice <ahjuice@users.noreply.github.com>
2018-07-09 14:06:50 -0400

    finished basic layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,63 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import {
+  Button,
+  Container,
+  Menu,
+  Segment,
+} from 'semantic-ui-react';
 
-import Navbar from './Navbar';
+import Welcome from './Welcome';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+
   render() {
+    const { fixed } = this.state;
+
     return (
-      <div id='main'>
-        <Navbar />
-        <h1>hello</h1>
+      <div id="main">
+        <Segment
+          inverted
+          textAlign='center'
+          style={{ minHeight: 700, padding: '1em 0em' }}
+          vertical
+        >
+          <Menu
+            fixed={fixed ? 'top' : null}
+            inverted={!fixed}
+            pointing={!fixed}
+            secondary={!fixed}
+            size='large'
+          >
+            <Container>
+              <Menu.Item as='a' active header>
+                  Tripr
+              </Menu.Item>
+              <Menu.Item position='right'>
+                <Button as='a' inverted={!fixed}>
+                    Log In
+                </Button>
+                <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                    Sign Up
+                </Button>
+              </Menu.Item>
+            </Container>
+          </Menu>
+          <Welcome />
+        </Segment>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  children: PropTypes.node,
+};
 
 export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {
+  Button,
+  Container,
+  Header,
+  Icon,
+} from 'semantic-ui-react';
+
+const Welcome = ({ mobile }) => (
+  <Container text>
+    <Header
+      as='h1'
+      content='Welcome to Tripr'
+      inverted
+      style={{
+        fontSize: mobile ? '2em' : '4em',
+        fontWeight: 'normal',
+        marginBottom: 0,
+        marginTop: mobile ? '1.5em' : '3em',
+      }}
+    />
+    <Header
+      as='h2'
+      content='Collaborative trip planning made easy.'
+      inverted
+      style={{
+        fontSize: mobile ? '1.5em' : '1.7em',
+        fontWeight: 'normal',
+        marginTop: mobile ? '0.5em' : '1.5em',
+      }}
+    />
+    <Button primary size='huge'>
+      Get Started
+      <Icon name='right arrow' />
+    </Button>
+  </Container>
+);
+
+Welcome.propTypes = {
+  mobile: PropTypes.bool,
+};
+
+export default Welcome;
